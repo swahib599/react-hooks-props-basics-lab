e6156a071767c094d8c84651946df4e9d6ba0764
Shaib Echesa Makokha <echesaswahib@gmail.com>
2024-07-31 17:43:03 +0300

    Updated About to use props for bio and links, adjusted tests to reflect changes, and passed props in App

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links"; // Make sure this is imported if you want to use it
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && <p>{bio}</p>} {/* Render bio if it exists */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin} /> {/* Render Links component with props */}
     </div>
   );
 }
